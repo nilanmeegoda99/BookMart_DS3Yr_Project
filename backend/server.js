ec23af6c5de702143b227781495a01936160360e
nilanmeegoda99 <nilanmeegoda@gmail.com>
2021-05-18 23:49:40 +0530

    add dotenv file to application

--- backend/server.js
+++ backend/server.js
@@ -19,4 +19,7 @@ app.get('/api/products/:id', (req, res) => {
   res.json(product)
 })
 
-app.listen(5000, console.log('Server running perfectly on port 5000'))
+//calling the dotenv file 
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
